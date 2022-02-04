79ee1f714431137d82a59ec93864b4a9cc581744
Satvik Vedala <72596484+satvikvedala@users.noreply.github.com>
2022-02-04 22:05:28 +0530

    Update and rename File.sql to Retrieve data from Tables.sql

--- w3_resources/Retrieve data from Tables.sql
+++ w3_resources/Retrieve data from Tables.sql
@@ -66,4 +66,68 @@ where (year = 1970) and (subject!='Physiology' and subject!='Economics');
 21.
 select *
 from nobel_win
-where (subject = 'Physiology' and year<
+where (subject = 'Physiology' and year<1971) or (subject = 'Peace' and year>=1974);
+
+22.
+select *
+from nobel_win
+where winner = 'Johannes Georg Bednorz';
+
+23.
+select *
+from nobel_win
+where subject not like 'P%';
+
+24.
+select *
+from nobel_win
+where year = 1970
+order by (subject != 'Chemistry' and subject!= 'Economics') desc, subject asc;
+
+25.
+select *
+from item_mast
+where (pro_price>=200 and pro_price<=600);
+
+26.
+select avg(PRO_PRICE)
+from item_mast
+where PRO_COM = 16;
+
+27.
+select pro_name as Item_Name, pro_price as Price_in_Rs
+from item_mast;
+
+28.
+select pro_name, pro_price
+from item_mast
+where pro_price>=250
+order by pro_price desc, pro_name asc;
+
+29.
+select pro_id, avg(pro_price)
+from item_mast
+group by pro_name;
+
+30.
+select pro_name, pro_price
+from item_mast
+order by pro_price asc;
+
+31.
+select distinct emp_lname
+from emp_details;
+
+32.
+select *
+from emp_details
+where emp_lname = 'Snares';
+
+33.
+select *
+from emp_details
+where emp_dept = 57;
+
+
+
+

346d4db2c738afd7d1f5ef71d6260c195ef931ed
Satvik Vedala <72596484+satvikvedala@users.noreply.github.com>
2022-02-05 23:36:19 +0530

    Update Wildcard and Special operators.sql

--- w3_resources/Wildcard and Special operators.sql
+++ w3_resources/Wildcard and Special operators.sql
@@ -26,6 +26,44 @@ where (commission>=0.12 and commission<=0.14);
 6.
 select *
 from orders
-where (purch(purch_amt!=948.50 and purch_amt!=1983.50);
+where (purch_amt>500 and purch_amt<4000) and (purch_amt != 948.50 and purch_amt != 1983.43);
        
 7.
+select *
+from salesman
+where (name between 'A%' and 'L%');
+
+8.
+select *
+from salesman
+where (name not between 'A%' and 'L%');
+
+9.
+select *
+from customer
+where cust_name like 'B%';
+
+10.
+select *
+from customer
+where cust_name like '%n';
+
+11.
+select *
+from salesman
+where name like 'N__l%';
+
+12.
+select *
+from testtable
+where col1 like '%/_%';
+
+13.
+select *
+from testtable
+where col1 not like '%/_%';
+
+14.
+select *
+from testtable
+where col1 like '%$/%' escape '$';

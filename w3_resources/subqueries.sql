a22424baea7b73a3573f4fdbed33d1411edc5b0c
Satvik Vedala <72596484+satvikvedala@users.noreply.github.com>
2022-02-17 21:39:20 +0530

    Update subqueries.sql

--- w3_resources/subqueries.sql
+++ w3_resources/subqueries.sql
@@ -1,1 +1,24 @@
+1.
+select o.ord_no,o.purch_amt,o.ord_date,o.customer_id,s.salesman_id
+from orders o
+left join salesman s on o.salesman_id = s.salesman_id
+where s.name = 'Paul Adam';
 
+2.
+select o.ord_no,o.purch_amt,o.ord_date,o.customer_id,s.salesman_id
+from orders o
+left join salesman s on o.salesman_id = s.salesman_id
+where s.city = 'London';
+
+3.
+select o.ord_no,o.purch_amt,o.ord_date,o.customer_id,s.salesman_id
+from orders o
+left join salesman s on o.salesman_id = s.salesman_id
+where o.customer_id = 3007;
+
+4.
+select avg(o.purch_amt)
+from orders o
+group by o.ord_date
+where o.ord_date = '2012-10-10';
+  
